c43bc0a873f3859daf990d820a377f7a8e5c2af2
3jackattack3 <3jackattack3@gmail.com>
2018-05-25 10:28:13 -0400

    working on the menu

--- src/Main.java
+++ src/Main.java
@@ -4,19 +4,49 @@
     5/23/2018
 */
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String args[]) {
-        Cipher encipherTest = new Encipher();
+//        Cipher encipherTest = new Encipher();
+//
+//        printMatrix(encipherTest.getCipher());
+//
+//        encipherTest.writeToFile("cipher.txt");
+//
+////        System.out.println("\n" + encipherTest.cipher("eee"));
+//
+//        Cipher decipherTest = new Decipher("cipher.txt");
+//
+//        System.out.println("\n" + decipherTest.cipher(encipherTest.cipher("hello my na1235me is jack")));  //encipherTest.cipher("eee")"6400049"
 
-        printMatrix(encipherTest.getCipher());
+        int choice = 0;
+        Scanner in = new Scanner(System.in);
+        Cipher encipher = null;
+        Cipher decipher = null;
+        String fileName;
+        String inputText;
 
-        encipherTest.writeToFile("cipher.txt");
 
-//        System.out.println("\n" + encipherTest.cipher("eee"));
+        while(choice >= 0 ){
+            System.out.println("1. create a random encipher\n2. create a standard encipher\n3. create an encipher from a text file\n4. enter text to encipher\n5. load a file with text to encipher\n6. create a random decipher\n7. create a standard decipher\n8. create a decipher from a text file\n9. enter text to decipher\n10. load a file with text to decipher\n-1. exit");
+            choice = Integer.parseInt(in.nextLine());
 
-        Cipher decipherTest = new Decipher("cipher.txt");
-
-        System.out.println("\n" + decipherTest.cipher(encipherTest.cipher("hello my na1235me is jack")));  //encipherTest.cipher("eee")"6400049"
+            switch(choice){
+                case 1: encipher = new Encipher(true);
+                        break;
+                case 2: encipher = new Encipher();
+                        break;
+                case 3: System.out.print("please enter the name of the file you want to load from: ");
+                        fileName = in.nextLine();
+                        encipher = new Encipher(fileName);
+                        break;
+                case 4: System.out.print("please enter text to encipher: ");
+                        inputText = in.nextLine();
+                        System.out.print(encipher.cipher(inputText));
+                default: break;
+                }
+        }
     }
 
     public static void printMatrix(char[][] matrix){
